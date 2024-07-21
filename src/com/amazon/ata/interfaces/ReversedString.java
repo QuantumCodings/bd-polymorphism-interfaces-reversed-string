0f1393da8ff0004f999ddd276ddea76263fea9b6
QuantumCodings <166426987+QuantumCodings@users.noreply.github.com>
2024-07-21 15:42:38 -0400

    Update ReversedString.java
    module 2

--- src/com/amazon/ata/interfaces/ReversedString.java
+++ src/com/amazon/ata/interfaces/ReversedString.java
@@ -1,19 +1,53 @@
 package com.amazon.ata.interfaces;
 
-/**
- * Reverse a String and act like a <code>CharSequence</code>.
- * <p>
- * Change this class definition to implement CharSequence.
- */
-public class ReversedString {
+public class ReversedString implements CharSequence {
 
+    private final String original;
 
-    /**
-     * Implement this constructor during the interfaces prework.
-     *
-     * @param chars The String of chars to be reversed
-     */
     public ReversedString(final String chars) {
+        this.original = chars;
     }
 
+    @Override
+    public int length() {
+        return original.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return original.charAt(original.length() - 1 - index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        
+        String substring = original.substring(original.length() - end, original.length() - start);
+        
+        StringBuilder reversed = new StringBuilder(substring).reverse();
+        return reversed.toString();
+    }
+
+    @Override
+    public String toString() {
+        
+        return new StringBuilder(original).reverse().toString();
+    }
+
+    
+    public static void main(String[] args) {
+        ReversedString reversedString = new ReversedString("cat");
+
+        
+        System.out.println(reversedString.charAt(0)); 
+
+        
+        System.out.println(reversedString.length()); 
+
+        
+        CharSequence subSequence = reversedString.subSequence(0, 2);
+        System.out.println(subSequence); 
+
+        
+        System.out.println(reversedString.toString()); 
+    }
 }
